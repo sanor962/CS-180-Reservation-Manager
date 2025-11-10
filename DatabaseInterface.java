39d7c89b2ea88c660976bd596880e1d7388d2053
Saanvi Verma <vermasaanvi962@gmail.com>
2025-11-10 14:18:24 -0500

    checkstyle errors

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -4,15 +4,17 @@ import java.util.List;
 /**
  * DatabaseInterface - Interface for Database class
  *
- * @author Saanvi Verma
+ * @author Saanvi Verma (verma279)
  * @version November 8, 2025
  */
 public interface DatabaseInterface {
-    boolean createAccount(String firstName, String lastName, int age, String userName, String password, String email, String phoneNumber);
+    boolean createAccount(String firstName, String lastName, int age, String userName, 
+                          String password, String email, String phoneNumber);
     boolean loginIntoAccount(String username, String password);
     boolean deleteAccount(String accountID, String userName, String password);
     Account getAccount(String accountID, String password);
-    String createReservation(String accountID, String showID, List<String> seatIDs, String date, String time, double totalPrice);
+    String createReservation(String accountID, String showID, List<String> seatIDs, 
+                             String date, String time, double totalPrice);
     boolean cancelReservation(String reservationID);
     ArrayList<Reservations> getReservationsByAccount(String accountID);
     Reservations getReservationByID(String reservationID);
