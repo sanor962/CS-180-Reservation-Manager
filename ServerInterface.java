0197602c52c53578bf5b04d5e8c10bad0c05f2dc
Saanvi Verma <vermasaanvi962@gmail.com>
2025-11-22 01:32:00 -0500

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -4,7 +4,7 @@ import java.util.List;
 /**
  * ServerInterface - Interface for Server class
  *
- * @author Kunj Arora (arora271)
+ * @author Kunj Arora (arora271) and Saanvi Verma (verma279)
  * @version November 21, 2025
  */
 
@@ -14,7 +14,7 @@ public interface ServerInterface {
                           String password, String email, String phoneNumber);
     boolean deleteAccount(String accountID, String username, String password);
     Account getAccount(String accountID, String password);
-    int createReservation(String accountID, String showID, List<String> seatIDs,
+    int createReservation(String accountID, String password, String showID, List<String> seatIDs,
                           String date, String time, double totalPrice);
     boolean cancelReservation(int reservationID);
     ArrayList<Reservations> getReservationsByAccount(String accountID);
@@ -22,5 +22,8 @@ public interface ServerInterface {
     Seat getSeat(String seatID);
     boolean reserveSeat(String seatID);
     boolean cancelSeat(String seatID);
-    ArrayList<Seat> getAvailableSeats();
+    ArrayList<Seat> getAvailableSeats(String showID, String date);
+    ArrayList<String> getAllConcerts();
+    boolean createConcert(String name, String date, String time);
+    String getTime(String concertID);
 }
