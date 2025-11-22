60e9c62dce03a1e095c28be8f22ee598992f8a56
Saanvi Verma <vermasaanvi962@gmail.com>
2025-11-22 01:37:13 -0500

    Create ConcertInterface.java

---
+++ ConcertInterface.java
@@ -0,0 +1,19 @@
+/**
+ * ConcertInterface - Interface for Concert class
+ *
+ * @author Saanvi Verma (verma279)
+ * @version November 22, 2025
+ */
+
+public interface ConcertInterface {
+    String getName();
+    String getDate();
+    String getTime();
+    int getID();
+    void setName(String name);
+    void setDate(String date);
+    void setTime(String time);
+    void setID(int ID);
+    String toString();
+    String writingInFile();
+}
